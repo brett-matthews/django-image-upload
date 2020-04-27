39bf5796779d3cb9bacc01a7108fb5bdcdd75e97
Brett Matthews <brett_matthews@live.com.au>
2020-04-27 17:43:48 +1000

    update region for privatemediastorage class
    Signed-off-by: Brett Matthews <brett_matthews@live.com.au>

--- imagedb/images/infrastructure.py
+++ imagedb/images/infrastructure.py
@@ -1,5 +1,11 @@
+from django.conf import  settings
+from storages.backends.s3boto3 import S3Boto3Storage
+
+
 class PrivateMediaStorage(S3Boto3Storage):
     location = settings.AWS_PRIVATE_MEDIA_LOCATION
     default_acl = 'private'
     file_overwrite = False
     custom_domain = False
+    region_name = settings.AWS_DEFAULT_REGION
+
