8606db65b55a5f64fad53391125d0f3be2d327a7
Brett Matthews <brett_matthews@live.com.au>
2020-04-28 09:17:52 +1000

    add s3 presigned url service
    Signed-off-by: Brett Matthews <brett_matthews@live.com.au>

--- imagedb/images/services.py
+++ imagedb/images/services.py
@@ -1,20 +1,43 @@
 import boto3
 
+from django.conf import settings
+
+from imagedb.images.models import Image
+
 
 class AwsRekognitionLabels(object):
 
     @staticmethod
-    def process_s3_object(s3_bucket, s3_key):
+    def get_labels(image: Image):
 
         client = boto3.client('rekognition')
 
         response = client.detect_labels(
             Image={
                 'S3Object': {
-                    'Bucket': s3_bucket,
-                    'Name': s3_key
+                    'Bucket': settings.AWS_STORAGE_BUCKET_NAME,
+                    'Name': image.image.name
                 }
             }
         )
 
         return response['Labels']
+
+
+class AwsS3PresignedUrl(object):
+
+    @staticmethod
+    def get_image_url(image: Image):
+
+        s3_client = boto3.client('s3')
+
+        response = s3_client.generate_presigned_url(
+            'get_object',
+            Params={
+                'Bucket': settings.AWS_STORAGE_BUCKET_NAME,
+                'Key': image.image.name
+            },
+            ExpiresIn=300
+        )
+
+        return response
