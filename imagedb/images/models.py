672259f07e379dea5b179953fb390c3248a29eef
Brett Matthews <brett_matthews@live.com.au>
2020-04-27 17:43:22 +1000

    update image storage attribute
    Signed-off-by: Brett Matthews <brett_matthews@live.com.au>

--- imagedb/images/models.py
+++ imagedb/images/models.py
@@ -1,13 +1,15 @@
 from time import time
 from django.db import models
 
+from imagedb.images.infrastructure import PrivateMediaStorage
+
 
 def user_directory_path(_, filename):
     return f'{round(time())}/{filename}'
 
 
 class Image(models.Model):
-    image = models.ImageField(upload_to=user_directory_path)
+    image = models.ImageField(upload_to=user_directory_path, storage=PrivateMediaStorage())
 
 
 class ImageLabel(models.Model):
